9a81134af3965562a426c7720778aef9c6e1d201
William Lohrmann <wlohrmann@oni.bio>
2020-12-26 20:15:13 +0000

    Add AoC styling - trying to learn CSS lol

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,21 @@ import styles from '../styles/Home.module.css'
 import Problems from '../src/problems'
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
+  return <>
+    <Head>
         <title>AoC Syllabus</title>
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+    </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          AoC Syllabus
-        </h1>
+    <main className={styles.main}>
+        <h1 className={styles["shiny-green"]}>AoC Syllabus</h1>
+        <Problems/>
 
       {/* "TODO: List of topics" } */}
-      <Problems/>
-      </main>
+    </main>
 
-      <footer className={styles.footer}>
+    <footer className={styles.footer}>
         <a href="https://nextjs.org/">Powered by Next.js</a>
-      </footer>
-    </div>
-  )
+    </footer>
+  </>
 }
--- src/problems.js
+++ src/problems.js
@@ -13,7 +13,6 @@ export default function Problems() {
         return problem.Title.toLowerCase().includes(filterInput.toLowerCase());
     }).sort((p1, p2) => {
         const cmp = p1[sortKey] - p2[sortKey];
-        const cmp2 = p1[sortKey].localeCompare(p2[sortKey]);
         return (ascending ? 1 : -1) * (!isNaN(cmp) ? cmp : (2 * p1[sortKey].localeCompare(p2[sortKey]) - 1));
     });
 
@@ -26,18 +25,22 @@ export default function Problems() {
         <table>
         <tbody>
         <tr>
-            {Object.keys(problems[0]).map(key => <th key={key}><button className={sortKey===key ? styles.selected : styles.button} onClick={() => {
+            {Object.keys(problems[0]).map(key => <th key={key} className={styles.highlightable} className={sortKey===key ? styles.selected : styles.button} onClick={() => {
                 if (sortKey == key) {
                     setAscending(!ascending);
                 } else {
                     setAscending(true);
                 }
                 return setSortKey(key);
-            }}>{key} {sortKey===key ? (ascending ? <i className="down arrow"/> : <i className="up arrow"/>) : ""}</button></th>)}
+            }}>{key} {sortKey===key ? (ascending ? <i className="down arrow"/> : <i className="up arrow"/>) : ""}</th>)}
         </tr>
         {filtered.map(problem => (
-            <tr key={problem.Title} className={styles.row}>
-                {Object.keys(problem).map(key => <td className={styles.element}>{key=="Title" ? <a href={`https://adventofcode.com/${problem["Year"]}/day/${problem["Day"]}`}>{problem[key]}</a> : problem[key]}</td>)}
+            <tr key={problem.Title} className={styles.highlightable}>
+                {Object.keys(problem).map(key => <td className={styles.element}>
+                    <a className={styles.problemLink} href={`https://adventofcode.com/${problem["Year"]}/day/${problem["Day"]}`}>
+                        {problem[key]}
+                    </a>
+                </td>)}
             </tr>
         ))}
         </tbody>
