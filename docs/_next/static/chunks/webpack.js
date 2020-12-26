637a5047f7bf55905106796f23f53beec9b77736
William Lohrmann <wlohrmann@oni.bio>
2020-12-26 10:39:16 +0000

    Build

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "35ae433ea07eea02c3a9";
+/******/ 	var hotCurrentHash = "75b06feeb51962c4a24b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
