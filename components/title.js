b28600829797a7a003264b99b5117b9d14baf99a
William Lohrmann <wlohrmann@oni.bio>
2020-12-27 11:56:55 +0000

    Restore glow

--- components/title.js
+++ components/title.js
@@ -1,3 +1,3 @@
 export default function Title(props) {
-    return <h1 style={{color:"#00cc00", "font-weight":"bold", "text-shadow":"0 0 1px #00cc00, 0 0 #00cc00"}}>{props.text}</h1>;
+    return <h1 style={{color:"#00cc00", "fontWeight":"bold", "textShadow":"0 0 3px #00cc00, 0 0 5px #00cc00"}}>{props.text}</h1>;
 }
